c0bd100961b4e2c00d3d67a1c5b2682caabbc15d
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-12-01 09:39:52 -0800

    adding basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <h1>Navbar</h1>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="/taco" element={<h1>taco page</h1>}/>
+          <Route path="/kitten/:id" element={<h1>one cat page</h1>}/>
+          <Route path="*" element={<h1>404 page</h1>}/>
+        </Routes>
+        <h1>Footer</h1>
+      </Router>
     </div>
   );
 }
